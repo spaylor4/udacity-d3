417709f7f68856ce2d7adfe5546ac35f6bd22dd1
spaylor4 <spaylor4@yahoo.com>
2020-01-24 16:14:24 -0500

    Create population scale for circle radius

--- l2-building-blocks/draw_circle.js
+++ l2-building-blocks/draw_circle.js
@@ -8,5 +8,13 @@ var svg = d3.select('.main').append('svg');
 svg.attr('width', 600).attr('height', 300);
 
 //create x and y scales for circle (video 22)
+// y is life expectancy, x is GDP per capita
 var y = d3.scale.linear().domain([15, 90]).range([0, 250]);
 var x = d3.scale.log().domain([250, 100000]).range([0, 600]);
+
+/*Note on circles in graphics (video 23)
+Be careful to use proportional areas rather than proportional radii to avoid
+exaggerating size differences by squaring the comparison quantity*/
+
+//create scale for radius of circle (video 24) representing population
+var r = d3.scale.sqrt().domain([52070, 1380000000]).range([10, 50]);
