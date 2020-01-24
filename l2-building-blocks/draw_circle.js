e1d62f89beb7503e493bbdf39f49f0c0b1ddbfbb
spaylor4 <spaylor4@yahoo.com>
2020-01-24 15:59:26 -0500

    Create x and y scales for plotting circle

--- l2-building-blocks/draw_circle.js
+++ l2-building-blocks/draw_circle.js
@@ -6,3 +6,7 @@ d3.select('.main').html('');
 var svg = d3.select('.main').append('svg');
 
 svg.attr('width', 600).attr('height', 300);
+
+//create x and y scales for circle (video 22)
+var y = d3.scale.linear().domain([15, 90]).range([0, 250]);
+var x = d3.scale.log().domain([250, 100000]).range([0, 600]);
